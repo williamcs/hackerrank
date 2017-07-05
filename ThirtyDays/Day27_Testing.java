2353bcaf8c96b9734458156ce3ee68bf49681ec5
will <solobird00@hotmail.com>
2017-07-05 10:08:19 +0800

    commit day 27 code.

---
+++ ThirtyDays/Day27_Testing.java
@@ -0,0 +1,42 @@
+package hackerrank.ThirtyDays;
+
+/**
+ * Created by will on 7/5/17.
+ */
+public class Day27_Testing {
+
+    public static void main(String[] args) {
+
+        System.out.println(5); // number of test case
+
+        System.out.println(3 + " " + 3);
+        System.out.println(0 + " " + -1 + " " + 1);
+
+        System.out.println(4 + " " + 2);
+        System.out.println(0 + " " + -1 + " " + 2 + " " + 1);
+
+        System.out.println(5 + " " + 3);
+        System.out.println(-1 + " " + 90 + " " + 999 + " " + 100 + " " + 0);
+
+        System.out.println(6 + " " + 1);
+        System.out.println(-1 + " " + 0 + " " + 1 + " " + -1 + " " + 2 + " " + 3);
+
+        System.out.println(7 + " " + 3);
+        System.out.println(-1 + " " + 0 + " " + 1 + " " + 2 + " " + 3 + " " + 4 + " " + 5);
+
+        /*System.out.println(3 + " " + 2);
+        System.out.println(0 + " " + -1 + " " + 1);
+
+        System.out.println(4 + " " + 2);
+        System.out.println(-1 + " " + 0 + " " + 1 + " " + 2);
+
+        System.out.println(5 + " " + 3);
+        System.out.println(-1 + " " + 0 + " " + 1 + " " + -1 + " " + 3);
+
+        System.out.println(6 + " " + 4);
+        System.out.println(-1 + " " + 0 + " " + 1 + " " + -1 + " " + 3 + " " + 5);
+
+        System.out.println(7 + " " + 3);
+        System.out.println(-1 + " " + 0 + " " + 1 + " " + -1 + " " + 3 + " " + 0 + " " + -1);*/
+    }
+}
